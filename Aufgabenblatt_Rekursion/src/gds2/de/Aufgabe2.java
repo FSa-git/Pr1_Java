3aa078a717e7f8bb3b9826b6da312d72c9e0c318
FS <francesco9721@gmail.com>
2023-01-31 15:08:37 +0100

    Create Aufgabe2.java

---
+++ Aufgabenblatt_Rekursion/src/gds2/de/Aufgabe2.java
@@ -0,0 +1,20 @@
+package gds2.de;
+
+public class Aufgabe2 {
+	
+	public static void main(String[] args) {
+		
+		String [] Spielerin = new String[12];
+		
+		Spielerin [0] = "Fe" + 12;
+		Spielerin [1] = "Es" + 16;
+		Spielerin [2] = "Sk" + 2;
+		
+		// auslesen 		
+		for (int i = 0; i < 2; i++) {
+			System.out.println(Spielerin[i]);
+		}
+		
+	}
+
+}
